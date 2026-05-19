d2e45f0b359ab10d2806758bb1ce959740ce6170
Gabriel 5illva <138382282+gabriel5illva@users.noreply.github.com>
2026-05-19 11:21:07 -0300

    Add listarPorPessoa method to ServicoProjeto

--- SERVICOS/ServicoProjeto.hpp
+++ SERVICOS/ServicoProjeto.hpp
@@ -5,44 +5,18 @@
 
 /**
  * @class ServicoProjeto
- * @brief Implementa os servicos relacionados a Projeto.
+ * @brief Implementa os servi&ccedil;os relacionados a Projeto.
  *
  * Esta classe realiza a interface IServiceProjeto e concentra
- * operacoes da camada de servico relacionadas a entidade Projeto.
+ * opera&ccedil;&otilde;es da camada de servi&ccedil;o relacionadas &agrave; entidade Projeto.
  */
 class ServicoProjeto : public IServiceProjeto {
 public:
-    /**
-     * @brief Cria um novo projeto.
-     *
-     * @param projeto Objeto da entidade Projeto.
-     * @return true se a operacao for realizada com sucesso.
-     */
     bool criar(const Projeto &projeto) override;
-
-    /**
-     * @brief Le um projeto a partir do codigo.
-     *
-     * @param codigo Objeto da classe de dominio Codigo.
-     * @return Objeto da entidade Projeto.
-     */
     Projeto ler(const Codigo &codigo) override;
-
-    /**
-     * @brief Atualiza os dados de um projeto.
-     *
-     * @param projeto Objeto da entidade Projeto.
-     * @return true se a operacao for realizada com sucesso.
-     */
     bool atualizar(const Projeto &projeto) override;
-
-    /**
-     * @brief Exclui um projeto a partir do codigo.
-     *
-     * @param codigo Objeto da classe de dominio Codigo.
-     * @return true se a operacao for realizada com sucesso.
-     */
     bool excluir(const Codigo &codigo) override;
+    bool listarPorPessoa(const Email &email) override;
 };
 
 #endif
