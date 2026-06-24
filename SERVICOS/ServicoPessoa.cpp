f125c93349825634cf6a0d6b8d6226012e77cef7
Gabriel 5illva <138382282+gabriel5illva@users.noreply.github.com>
2026-06-24 14:02:04 -0300

    Implement ServicoPessoa class for managing Pessoa

---
+++ SERVICOS/ServicoPessoa.cpp
@@ -0,0 +1,45 @@
+#include "../../Headers/SERVICOS/ServicoPessoa.hpp"
+
+ServicoPessoa::ServicoPessoa() {
+    containerPessoa = nullptr;
+}
+
+void ServicoPessoa::setContainerPessoa(ContainerPessoa *containerPessoa) {
+    this->containerPessoa = containerPessoa;
+}
+
+bool ServicoPessoa::criar(const Pessoa &pessoa) {
+    if (containerPessoa == nullptr) {
+        return false;
+    }
+
+    return containerPessoa->inserir(pessoa);
+}
+
+Pessoa ServicoPessoa::ler(const Email &email) {
+    Pessoa pessoa;
+
+    if (containerPessoa == nullptr) {
+        return pessoa;
+    }
+
+    containerPessoa->buscar(email, pessoa);
+
+    return pessoa;
+}
+
+bool ServicoPessoa::atualizar(const Pessoa &pessoa) {
+    if (containerPessoa == nullptr) {
+        return false;
+    }
+
+    return containerPessoa->atualizar(pessoa);
+}
+
+bool ServicoPessoa::excluir(const Email &email) {
+    if (containerPessoa == nullptr) {
+        return false;
+    }
+
+    return containerPessoa->remover(email);
+}
